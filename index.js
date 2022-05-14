c8d60d653bb3ed86fdd2ecab43b93a3859959a5a
unknown <katungaarmand1@gmail.com>
2022-05-14 19:41:26 +0100

    here is my front-end inplemantation using ejs as engine

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const path = require('path')
 
 const app = express()
 
+app.use(bodyparser.urlencoded({extended:true}))
+app.set("view engine","ejs")
 
-
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')))
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')))
 const port = process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.listen(port,()=>{
     console.log(`am listenning on http://localhost:${port}`)
